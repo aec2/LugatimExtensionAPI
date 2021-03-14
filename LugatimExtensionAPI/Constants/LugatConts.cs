e789bfd966d9d7a95163b16505959da156b7d875
Enes Can <enes.can@kc.com.tr>
2021-03-14 16:05:41 +0300

    Add project files.

---
+++ LugatimExtensionAPI/Constants/LugatConts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LugatimExtensionAPI.Constants
+{
+    public static class LugatConts
+    {
+        public const string LugatBaseUrl = "http://lugatim.com/s/";
+    }
+}
---
+++ LugatimExtensionAPI/Controllers/LugatController.cs
@@ -0,0 +1,33 @@
+﻿using LugatimExtensionAPI.Constants;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace LugatimExtensionAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LugatController : ControllerBase
+    {
+
+
+        [HttpGet("{word}")]
+        public async Task<string> Get(string word)
+         {
+            var httpClient = new HttpClient();
+            HttpResponseMessage result = await httpClient.GetAsync(string.Concat(LugatConts.LugatBaseUrl, word));
+            var response = await result.Content.ReadAsStringAsync();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(response);
+
+            var htmlBody = doc.DocumentNode.SelectNodes("/html/");
+
+
+            return htmlBody.FirstOrDefault() != null ? htmlBody.FirstOrDefault().InnerText.Trim() : "CIKMAZ SOKAK";
+        }
+    }
+}
